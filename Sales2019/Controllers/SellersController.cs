b3b12f20f01d34d0d4dced54a6d6f3cfeb3f0ec8
Lucas silveira <lukas.silveira@hotmail.com>
2019-01-22 14:33:31 -0200

    SellerFormViewModel and Department select component

--- Sales2019/Controllers/SellersController.cs
+++ Sales2019/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Sales2019.Models;
+using Sales2019.Models.ViewModels;
 using Sales2019.Services;
 
 namespace Sales2019.Controllers
@@ -11,10 +12,13 @@ namespace Sales2019.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
 
@@ -26,7 +30,9 @@ namespace Sales2019.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ Sales2019/Services/DepartmentService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sales2019.Models;
+
+namespace Sales2019.Services
+{
+    public class DepartmentService
+    {
+
+        private readonly Sales2019Context _context;
+
+        public DepartmentService(Sales2019Context context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+    }
+}
