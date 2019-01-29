1c038576471213cd1a402030e1b2e640f0edeab1
Lucas silveira <lukas.silveira@hotmail.com>
2019-01-29 14:25:54 -0200

    App locale, number and date formatting

--- Sales2019/Models/Seller.cs
+++ Sales2019/Models/Seller.cs
@@ -1,5 +1,6 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,15 @@ namespace Sales2019.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email  { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
-        public double BaseSalary{ get; set; }
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
@@ -41,7 +48,8 @@ namespace Sales2019.Models
             Sales.Remove(sr);
         }
 
-        public double TotalSales(DateTime initial ,DateTime final) {
+        public double TotalSales(DateTime initial, DateTime final)
+        {
             return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
         }
     }
